59f9c0928dcd73674906cae1f659f3728a379fcd
Devansh Shukla <devanshc.shukla@gmail.com>
2026-01-27 13:47:29 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,8 @@ import shutil
 import os
 import json
 import uvicorn
-from utils import process_handwritten_images
-from agent_logic import verify
+from backend.utils import process_handwritten_images
+from backend.agent_logic import verify
 
 app = FastAPI()
 
